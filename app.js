16299da46d1ead190380e99bb0819deaaa7bdcf8
Rey <rbuciodev@gmail.com>
2017-11-07 14:24:35 -0800

    install mongoose and created campground schema

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
 
-let campgrounds = [
-    { name: 'Rough Trails', image: 'https://images.unsplash.com/photo-1479244209311-71e35c910f59?auto=format&fit=crop&w=1350&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' },
-    { name: 'King of the Hill', image: 'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?auto=format&fit=crop&w=1350&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' },
-    { name: 'Springfields', image: 'https://images.unsplash.com/photo-1485216983937-749292830fcf?auto=format&fit=crop&w=1374&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' },
-    { name: 'Nowhere Creek', image: 'https://images.unsplash.com/photo-1484960055659-a39d25adcb3c?auto=format&fit=crop&w=1350&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' },
-    { name: 'La Loma Hills', image: 'https://images.unsplash.com/photo-1499363536502-87642509e31b?auto=format&fit=crop&w=634&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' },
-    { name: 'River Dale Camp', image: 'https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?auto=format&fit=crop&w=1350&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' }
-];
+// CAMPGROUND SCHEMA
+mongoose.connect('mongodb://localhost/yelpCamp',{useMongoClient: true}, function() {
+    console.log('Connection to database successful');
+});
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -20,23 +25,37 @@ app.get('/', function(req, res) {
 });
 
 app.get('/campgrounds', function(req, res) {
-    res.render('campgrounds', {campgrounds: campgrounds});
+    // GET ALL CAMPGROUNDS
+    Campground.find({}, function(err, campgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('campgrounds', {campgrounds: campgrounds});
+        }
+    });
 });
 
 app.post('/campgrounds', function(req, res) {
-    // get data from form and add to campgrounds array
 
+    // get data from form and add to campgrounds array
     let name = req.body.name;
     let image = req.body.image;
-    let campground = {
+    let newCampground = {
         name: name,
         image: image
     }
-    campgrounds.push(campground);
-    console.log(campgrounds);
-    res.redirect('/campgrounds');
 
-    // redirect back to campgrounds
+    // CREATE CAMPGROUND
+    Campground.create(newCampground, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            // redirect back to campgrounds
+            res.redirect('/campgrounds');
+        }
+    })
+
+
 });
 
 app.get('/campgrounds/new', function(req, res) {
