26a053f105ff30bdc8125e0c6728c8346d151b15
Rey <rbuciodev@gmail.com>
2017-11-06 19:34:37 -0800

    added initial routes and view files

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('landing');
+});
+
+app.get('/campgrounds', function(req, res) {
+    let campgrounds = [
+        { name: 'Rough Trails', image: '' },
+        { name: 'King of the Hill', image: '' },
+        { name: 'Springfields', image: '' },
+        { name: 'Nowhere Creek', image: '' },
+        { name: 'La Loma Hills', image: '' },
+        { name: 'River Dale Camp', image: '' }
+    ];
+    res.render('campgrounds', {campgrounds: campgrounds});
+});
+
 app.listen(3000, function() {
     console.log('Yelpcamp Server is running!!!');
 });
