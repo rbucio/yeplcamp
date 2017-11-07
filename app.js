ce1e90e740a0249b37589b2ce09fb30f4621a880
Rey <rbuciodev@gmail.com>
2017-11-06 19:49:52 -0800

    added partials, header and footer

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.get('/', function(req, res) {
 
 app.get('/campgrounds', function(req, res) {
     let campgrounds = [
-        { name: 'Rough Trails', image: '' },
-        { name: 'King of the Hill', image: '' },
-        { name: 'Springfields', image: '' },
-        { name: 'Nowhere Creek', image: '' },
-        { name: 'La Loma Hills', image: '' },
-        { name: 'River Dale Camp', image: '' }
+        { name: 'Rough Trails', image: 'https://images.unsplash.com/photo-1479244209311-71e35c910f59?auto=format&fit=crop&w=1350&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' },
+        { name: 'King of the Hill', image: 'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?auto=format&fit=crop&w=1350&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' },
+        { name: 'Springfields', image: 'https://images.unsplash.com/photo-1485216983937-749292830fcf?auto=format&fit=crop&w=1374&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' },
+        { name: 'Nowhere Creek', image: 'https://images.unsplash.com/photo-1484960055659-a39d25adcb3c?auto=format&fit=crop&w=1350&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' },
+        { name: 'La Loma Hills', image: 'https://images.unsplash.com/photo-1499363536502-87642509e31b?auto=format&fit=crop&w=634&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' },
+        { name: 'River Dale Camp', image: 'https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?auto=format&fit=crop&w=1350&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' }
     ];
     res.render('campgrounds', {campgrounds: campgrounds});
 });
